60175eeb26d7941e69ccb59a2663040170d00270
Miguel Angel Hurtado Cespedes <hurtado.miguel.c.o@gmail.com>
2017-02-20 12:08:35 -0400

    Initial Commit

---
+++ EjercicioIconos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjercicioIconos
+//
+//  Created by Miguel Angel Hurtado on 20/2/17.
+//  Copyright © 2017 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

498ddf8845f92f1eec2c649fd4b8905cf82214b8
Miguel Angel Hurtado Cespedes <hurtado.miguel.c.o@gmail.com>
2017-02-20 12:36:02 -0400

    nuevos

--- EjercicioIconos/ViewController.swift
+++ EjercicioIconos/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var lastNameText: UITextField!
+    
+    
+    @IBOutlet weak var helloText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +26,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func sayHelloAction(_ sender: Any) {
+        
+        self.helloText.text = "hola \(self.nameText.text!) \(self.lastNameText.text!)"
     }
 
 
